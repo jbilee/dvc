1baf785bda5c7f4cb9a04132ec9a1aef84abe346
jbilee <julishahn@gmail.com>
2023-12-29 20:30:11 +0900

    Finished logic for reverse calculator

--- calc/revcalc.js
+++ calc/revcalc.js
@@ -10,96 +10,124 @@ function getChangedFields() {
     const field = Number(document.getElementById(selector).value);
     fields.push(field);
   });
-
+  const requirements = {};
   // read dragon name field
   const dragonName = document.getElementById("dragon-selector").value;
-  const currentHighest = getCheckedTraits();
+  const checkedHighest = getCheckedTraits();
   const currentTrait = document.getElementById("trait-selector").value;
-  const currentLowest = normalTraits.filter((trait) => trait.nameEn === currentTrait).map((trait) => trait.lowestTrait);
+  requirements.lowest = normalTraits.filter((trait) => trait.nameEn === currentTrait).map((trait) => trait.lowestTrait).toString();
+  requirements.highest = normalTraits.filter((trait) => trait.nameEn === currentTrait).map((trait) => trait.highestTrait).toString();
+
+  // Early return 1: user input error (personality user selected and checkboxes don't match)
+  if (!checkedHighest.includes(requirements.highest)) {
+    console.log("impossible")
+    return
+  }
 
   // get dragon's trait data
   const dragonTraits = {};
   dragonList.forEach((data) => {
     if (data.name.includes(dragonName)) {
-      dragonTraits.first = findTraitData(data.traitsKo[0]);
-      dragonTraits.first.values = data[data.traitsEn[0]];
-      dragonTraits.second = findTraitData(data.traitsKo[1]);
-      dragonTraits.second.values = data[data.traitsEn[1]];
+      dragonTraits.firstTrait = findTraitData(data.traitsKo[0]);
+      dragonTraits.firstTrait.baseStats = new Stats(...data[data.traitsEn[0]]);
+      dragonTraits.secondTrait = findTraitData(data.traitsKo[1]);
+      dragonTraits.secondTrait.baseStats = new Stats(...data[data.traitsEn[1]]);
     }
   });
   console.log("dragonTraits ---");
   console.log(dragonTraits);
 
-  const changes = new Stats(...fields);
-  const test1 = new Stats(...dragonTraits.first.values);
-  const test2 = new Stats(...dragonTraits.second.values);
+  const trainAmount = new Stats(...fields);
 
   console.log("base stats ---------");
-  console.log(test1);
-  console.log(test2);
+  console.log(dragonTraits.firstTrait.baseStats);
+  console.log(dragonTraits.secondTrait.baseStats);
 
   // add the affected stats to base stats
-  const added1 = getStatSum(test1, changes);
-  const added2 = getStatSum(test2, changes);
+  dragonTraits.firstTrait.trainedStats = getStatSum(dragonTraits.firstTrait.baseStats, trainAmount);
+  dragonTraits.secondTrait.trainedStats = getStatSum(dragonTraits.secondTrait.baseStats, trainAmount);
 
-  console.log("added stats ---------");
-  console.log(added1);
-  console.log(added2);
+  console.log("stats after training ---------");
+  console.log(dragonTraits.firstTrait.trainedStats);
+  console.log(dragonTraits.secondTrait.trainedStats);
 
   // find final trait based on the changes
-  const highest1 = added1.getMaxStatName();
-  const highest2 = added2.getMaxStatName();
-  console.log("theoretical higehst ---------");
-  console.log(highest1);
-  console.log(highest2);
+  dragonTraits.firstTrait.trainedHighest = dragonTraits.firstTrait.trainedStats.getMaxStatName();
+  dragonTraits.secondTrait.trainedHighest = dragonTraits.secondTrait.trainedStats.getMaxStatName();
 
-  // check whether current highest matches theoretical highest
-  const lowestStats1 = findLowestStatName(added1);
-  const lowestStats2 = findLowestStatName(added2);
-
-  console.log("theoretical lowest ---------");
-  console.log(lowestStats1);
-  console.log(lowestStats2);
-
-  console.log(currentLowest)
-
-  let matches1;
-  let matches2;
-
-  for (let i = 0; i < currentHighest.length; i++) {
-    if (!highest1.includes(currentHighest[i])) {
-      matches1 = false;
-      break;
-    }
-    if (i === currentHighest.length - 1 && highest1.includes(currentHighest[i])) {
-      matches1 = true;
+  for (const trait in dragonTraits) {
+    const minStats = dragonTraits[trait].trainedStats.getMinStatName();
+    if (minStats.length > 1) {
+      dragonTraits[trait].trainedLowest = "none";
+    } else {
+      dragonTraits[trait].trainedLowest = minStats.toString();
     }
   }
 
-  for (let i = 0; i < currentHighest.length; i++) {
-    if (!highest2.includes(currentHighest[i])) {
-      matches2 = false;
-      break;
-    }
-    if (i === currentHighest.length - 1 && highest2.includes(currentHighest[i])) {
-      matches2 = true;
-    }
+  console.log(requirements)
+
+  const resultSpan = document.createElement("div");
+
+  if (dragonTraits.firstTrait.trainedHighest.includes(requirements.highest) && dragonTraits.firstTrait.trainedLowest === requirements.lowest) {
+    resultSpan.innerText = dragonTraits.firstTrait.trait;
+  } else if (dragonTraits.secondTrait.trainedHighest.includes(requirements.highest) && dragonTraits.secondTrait.trainedLowest === requirements.lowest) {
+    resultSpan.innerText = dragonTraits.secondTrait.trait;
+  } else {
+    // exception handling
+    console.log("impossible")
   }
 
-  if (lowestStats1 !== currentLowest[0]) {
-    matches1 = false;
-  }
+  const mama = document.getElementById("controls");
+  mama.appendChild(resultSpan)
 
-  if (lowestStats2 !== currentLowest[0]) {
-    matches2 = false;
-  }
+  // let matches1;
+  // let matches2;
 
-  console.log(matches1) // expect false
-  console.log(matches2) // expect true
+  // for (let i = 0; i < checkedHighest.length; i++) {
+  //   if (!highest1.includes(checkedHighest[i])) {
+  //     matches1 = false;
+  //     break;
+  //   }
+  //   if (i === checkedHighest.length - 1 && highest1.includes(checkedHighest[i])) {
+  //     matches1 = true;
+  //   }
+  // }
+
+  // for (let i = 0; i < checkedHighest.length; i++) {
+  //   if (!highest2.includes(checkedHighest[i])) {
+  //     matches2 = false;
+  //     break;
+  //   }
+  //   if (i === checkedHighest.length - 1 && highest2.includes(checkedHighest[i])) {
+  //     matches2 = true;
+  //   }
+  // }
+
+  // if (lowestStats1 !== requirements.lowest[0]) {
+  //   matches1 = false;
+  // }
+
+  // if (lowestStats2 !== requirements.lowest[0]) {
+  //   matches2 = false;
+  // }
+
+  // console.log(matches1) // expect false
+  // console.log(matches2) // expect true
+
+  /*
+유저 인풋:
+가장 높은 스텟
+현재 성격
+
+if there's a tie in highest, 마지막으로 올린 스탯을 기준으로 새로 잡음
+
+가장 높은 스탯들이 다 일치하는지 확인
+
+  */
 
   // test data:
   // { name: ["Knight Dragon", "나이트"], traitsKo: ["용감한", "똑똑한"], Brave: [0, 30, 10, 10], Smart: [10, 0, 0, 20] },
-  // 나이트 순발+40, 성격 대담
+  // 나이트 순발+10, 성격 대담
   // 1 to match
   // expect: 성격 눈치빠른
 
@@ -149,17 +177,6 @@ function getCheckedTraits() {
     }
   });
 
-  // const agilityCheckbox = document.getElementById("checkbox-agility").checked;
-  // const strengthCheckbox = document.getElementById("checkbox-strength").checked;
-  // const focusCheckbox = document.getElementById("checkbox-focus").checked;
-  // const intellectCheckbox =
-  //   document.getElementById("checkbox-intellect").checked;
-
-  // console.log("agility checked: " + agilityCheckbox);
-  // console.log("strength checked: " + strengthCheckbox);
-  // console.log("focus checked: " + focusCheckbox);
-  // console.log("intellect checked: " + intellectCheckbox);
-console.log(checked);
   return checked;
 }
 
