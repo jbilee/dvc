8917a7be1f9ffad74f22b3e1dfc1639b5d7c9eb2
jbilee <julishahn@gmail.com>
2023-12-30 19:57:29 +0900

    Refactored code - Erased commented out lines of code in revcalc.js - Used return instead of else statement in `detectBase` method - Changed the layout of reverse calculator and revised tooltips

--- calc/revcalc.js
+++ calc/revcalc.js
@@ -34,22 +34,12 @@ function getChangedFields() {
       dragonTraits.secondTrait.baseStats = new Stats(...data[data.traitsEn[1]]);
     }
   });
-  console.log("dragonTraits ---");
-  console.log(dragonTraits);
 
   const trainAmount = new Stats(...fields);
-
-  console.log("base stats ---------");
-  console.log(dragonTraits.firstTrait.baseStats);
-  console.log(dragonTraits.secondTrait.baseStats);
 
   // add the affected stats to base stats
   dragonTraits.firstTrait.trainedStats = getStatSum(dragonTraits.firstTrait.baseStats, trainAmount);
   dragonTraits.secondTrait.trainedStats = getStatSum(dragonTraits.secondTrait.baseStats, trainAmount);
-
-  console.log("stats after training ---------");
-  console.log(dragonTraits.firstTrait.trainedStats);
-  console.log(dragonTraits.secondTrait.trainedStats);
 
   // find final trait based on the changes
   dragonTraits.firstTrait.trainedHighest = dragonTraits.firstTrait.trainedStats.getMaxStatName();
@@ -64,82 +54,16 @@ function getChangedFields() {
     }
   }
 
-  console.log(requirements)
-
-  const resultSpan = document.createElement("div");
+  const resultDiv = document.getElementById("result");
 
   if (dragonTraits.firstTrait.trainedHighest.includes(requirements.highest) && dragonTraits.firstTrait.trainedLowest === requirements.lowest) {
-    resultSpan.innerText = dragonTraits.firstTrait.trait;
+    resultDiv.innerText = dragonTraits.firstTrait.trait;
   } else if (dragonTraits.secondTrait.trainedHighest.includes(requirements.highest) && dragonTraits.secondTrait.trainedLowest === requirements.lowest) {
-    resultSpan.innerText = dragonTraits.secondTrait.trait;
+    resultDiv.innerText = dragonTraits.secondTrait.trait;
   } else {
     // exception handling
     console.log("impossible")
   }
-
-  const mama = document.getElementById("controls");
-  mama.appendChild(resultSpan)
-
-  // let matches1;
-  // let matches2;
-
-  // for (let i = 0; i < checkedHighest.length; i++) {
-  //   if (!highest1.includes(checkedHighest[i])) {
-  //     matches1 = false;
-  //     break;
-  //   }
-  //   if (i === checkedHighest.length - 1 && highest1.includes(checkedHighest[i])) {
-  //     matches1 = true;
-  //   }
-  // }
-
-  // for (let i = 0; i < checkedHighest.length; i++) {
-  //   if (!highest2.includes(checkedHighest[i])) {
-  //     matches2 = false;
-  //     break;
-  //   }
-  //   if (i === checkedHighest.length - 1 && highest2.includes(checkedHighest[i])) {
-  //     matches2 = true;
-  //   }
-  // }
-
-  // if (lowestStats1 !== requirements.lowest[0]) {
-  //   matches1 = false;
-  // }
-
-  // if (lowestStats2 !== requirements.lowest[0]) {
-  //   matches2 = false;
-  // }
-
-  // console.log(matches1) // expect false
-  // console.log(matches2) // expect true
-
-  /*
-유저 인풋:
-가장 높은 스텟
-현재 성격
-
-if there's a tie in highest, 마지막으로 올린 스탯을 기준으로 새로 잡음
-
-가장 높은 스탯들이 다 일치하는지 확인
-
-  */
-
-  // test data:
-  // { name: ["Knight Dragon", "나이트"], traitsKo: ["용감한", "똑똑한"], Brave: [0, 30, 10, 10], Smart: [10, 0, 0, 20] },
-  // 나이트 순발+10, 성격 대담
-  // 1 to match
-  // expect: 성격 눈치빠른
-
-  // both highest and lowest have to match
-
-  // exception handling -- maybe I don't need to do this; completely depends on info provided by user
-
-  // need to consider ties in stats -> change dropdown to checkbox
-
-  // const trueTrait;
-  // const resultDiv = document.getElementById("result");
-  // resultDiv.innerHTML = trueTrait;
 }
 
 function findLowestStatName(stats) {
