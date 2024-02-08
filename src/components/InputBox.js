24e2f40dbbb2f1ce36182fc2ad9615795678c2dc
jbilee <julishahn@gmail.com>
2024-02-09 01:27:27 +0900

    feat: Customizable input field - An input field that can have uniform styles across all devices

---
+++ src/components/InputBox.js
@@ -0,0 +1,71 @@
+class InputBox extends HTMLElement {
+  static observedAttributes = ["boxlength", "boxcolor", "cap"];
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: "open" });
+
+    const wrapper = document.createElement("div");
+    wrapper.setAttribute("class", "wrapper");
+
+    const container = document.createElement("div");
+    container.setAttribute("class", "container");
+    container.innerHTML = `<input class="input-field" type="number" inputmode="numeric" min="0" value="0">`;
+
+    wrapper.append(container);
+    shadow.append(wrapper);
+
+    const style = document.createElement("style");
+    style.textContent = `
+      .wrapper {
+        box-sizing: border-box;
+      }
+      .container {
+        border-radius: 6px;
+        background: rgb(204, 204, 204);
+        padding: 6px 8px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      input {
+        background: transparent;
+        border: none;
+        width: 100%;
+        height: 100%;
+      }
+    `;
+
+    shadow.append(style);
+  }
+
+  attributeChangedCallback(attribute, oldValue, newValue) {
+    if (this.shadowRoot === null) return;
+
+    const shadow = this.shadowRoot;
+
+    switch (attribute) {
+      case "boxlength": {
+        const container = shadow.querySelector(".wrapper");
+        container.setAttribute("style", `width:${newValue};`);
+        return;
+      }
+      case "boxcolor": {
+        const container = shadow.querySelector(".container");
+        container.setAttribute("style", `background:${newValue};`);
+        return;
+      }
+      case "cap": {
+        const inputField = shadow.querySelector("input");
+        inputField.setAttribute("max", newValue);
+        return;
+      }
+      default:
+    }
+  }
+}
+
+export default InputBox;
