a923d24b6fb4c1ed6a753a14fbf61f004d4aa070
jbilee <julishahn@gmail.com>
2024-02-29 20:22:03 +0900

    fix: Data fix

--- src/td.js
+++ src/td.js
@@ -12,6 +12,25 @@ const specialTraits = [
 ];
 
 const normalTraits = [
+  { nameEn: "Naive", nameKo: "천진난만한", highestStat: "agility", lowestStat: "strength" },
+  { nameEn: "Rash", nameKo: "덜렁대는", highestStat: "agility", lowestStat: "focus" },
+  { nameEn: "Hasty", nameKo: "성급한", highestStat: "agility", lowestStat: "intellect" },
+  { nameEn: "quickWitted", nameKo: "눈치빠른", highestStat: "agility", lowestStat: "none" },
+  { nameEn: "Brave", nameKo: "용감한", highestStat: "strength", lowestStat: "agility" },
+  { nameEn: "Quirky", nameKo: "변덕쟁이", highestStat: "strength", lowestStat: "focus" },
+  { nameEn: "Adamant", nameKo: "고집있는", highestStat: "strength", lowestStat: "intellect" },
+  { nameEn: "Bold", nameKo: "대담한", highestStat: "strength", lowestStat: "none" },
+  { nameEn: "Quiet", nameKo: "냉정한", highestStat: "focus", lowestStat: "agility" },
+  { nameEn: "Calm", nameKo: "차분한", highestStat: "focus", lowestStat: "strength" },
+  { nameEn: "Careful", nameKo: "신중한", highestStat: "focus", lowestStat: "intellect" },
+  { nameEn: "Hardy", nameKo: "노력하는", highestStat: "focus", lowestStat: "none" },
+  { nameEn: "Docile", nameKo: "온순한", highestStat: "intellect", lowestStat: "agility" },
+  { nameEn: "Bashful", nameKo: "수줍은", highestStat: "intellect", lowestStat: "strength" },
+  { nameEn: "Lax", nameKo: "촐랑대는", highestStat: "intellect", lowestStat: "focus" },
+  { nameEn: "Smart", nameKo: "똑똑한", highestStat: "intellect", lowestStat: "none" }
+];
+
+const normalTraitsv2 = [
   { nameEn: "Docile", nameKo: "온순한", reqValue: "lowest", reqStat: "agility", hasSameStats: false },
   { nameEn: "Calm", nameKo: "차분한", reqValue: "lowest", reqStat: "agility", hasSameStats: true },
   { nameEn: "Naive", nameKo: "천진난만한", reqValue: "lowest", reqStat: "strength", hasSameStats: false },
