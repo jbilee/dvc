d84fab0860554a1c78142968b25c3c11d1ae3766
jbilee <julishahn@gmail.com>
2025-06-18 16:15:54 +0900

    Apply manual user data fix

--- src/models/App.js
+++ src/models/App.js
@@ -23,17 +23,207 @@ class App {
   fixUserData() {
     const storageData = JSON.parse(localStorage.getItem("dvcfvs"));
     if (!storageData || storageData.length <= 0) return;
-    const faultyData1 = storageData.find((obj) => obj.nameKo === "페이몬");
-    const faultyData2 = storageData.find((obj) => obj.nameEn === "Haemagon");
+    const faultyData1 = storageData.find((obj) => obj.nameEn === "Gandharba");
+    const faultyData2 = storageData.find((obj) => obj.nameEn === "Gautling-Gon");
+    const faultyData3 = storageData.find((obj) => obj.nameEn === "Gold Pike");
+    const faultyData4 = storageData.find((obj) => obj.nameEn === "Devilgon");
+    const faultyData5 = storageData.find((obj) => obj.nameEn === "Lattegon");
+    const faultyData6 = storageData.find((obj) => obj.nameEn === "Lalux");
+    const faultyData7 = storageData.find((obj) => obj.nameEn === "Lambgon");
+    const faultyData8 = storageData.find((obj) => obj.nameEn === "Rose Gold");
+    const faultyData9 = storageData.find((obj) => obj.nameEn === "Libro");
+    const faultyData10 = storageData.find((obj) => obj.nameEn === "Manimekalai");
+    const faultyData11 = storageData.find((obj) => obj.nameEn === "Mushroom Knight");
+    const faultyData12 = storageData.find((obj) => obj.nameEn === "Mystictiny");
+    const faultyData13 = storageData.find((obj) => obj.nameEn === "Hedgebat");
+    const faultyData14 = storageData.find((obj) => obj.nameEn === "Venom");
+    const faultyData15 = storageData.find((obj) => obj.nameEn === "Vonsharkgon");
+    const faultyData16 = storageData.find((obj) => obj.nameEn === "Bonehead");
+    const faultyData17 = storageData.find((obj) => obj.nameEn === "Shadow");
+    const faultyData18 = storageData.find((obj) => obj.nameEn === "Serpent Flower");
+    const faultyData19 = storageData.find((obj) => obj.nameEn === "Skelegon");
+    const faultyData20 = storageData.find((obj) => obj.nameEn === "Six-leg Horn");
+    const faultyData21 = storageData.find((obj) => obj.nameEn === "Iguagon");
+    const faultyData22 = storageData.find((obj) => obj.nameEn === "Caracen");
+    const faultyData23 = storageData.find((obj) => obj.nameEn === "Catsgon");
+    const faultyData24 = storageData.find((obj) => obj.nameEn === "Qualle");
+    const faultyData25 = storageData.find((obj) => obj.nameEn === "Tutankhagon");
+    const faultyData26 = storageData.find((obj) => obj.nameEn === "Pharaogon");
+    const faultyData27 = storageData.find((obj) => obj.nameEn === "Enoki Mushroom Dragon");
+    const faultyData28 = storageData.find((obj) => obj.nameEn === "White Gold");
+    const faultyData29 = storageData.find((obj) => obj.nameEn === "Hydragon");
     if (faultyData1) {
-      console.log("Fixing 페이몬...");
-      const index = storageData.findIndex((obj) => obj.nameKo === "페이몬");
-      storageData[index].nameKo = "파이몬";
+      console.log("Fixing Gandharba...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Gandharba");
+      storageData[index].nameEn = "Gandharva";
       localStorage.setItem("dvcfvs", JSON.stringify(storageData));
-    } else if (faultyData2) {
-      console.log("Fixing Haemagon...");
-      const index = storageData.findIndex((obj) => obj.nameEn === "Haemagon");
-      storageData[index].nameEn = "Seahorse Dragon";
+    }
+    if (faultyData2) {
+      console.log("Fixing Gautling-Gon...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Gautling-Gon");
+      storageData[index].nameEn = "Gatling-Gun Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData3) {
+      console.log("Fixing Gold Pike...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Gold Pike");
+      storageData[index].nameEn = "Goldpike";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData4) {
+      console.log("Fixing Devilgon...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Devilgon");
+      storageData[index].nameEn = "Devil Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData5) {
+      console.log("Fixing Lattegon...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Lattegon");
+      storageData[index].nameEn = "Latte Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData6) {
+      console.log("Fixing Lalux...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Lalux");
+      storageData[index].nameEn = "La Lux";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData7) {
+      console.log("Fixing Lambgon...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Lambgon");
+      storageData[index].nameEn = "Lamb Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData8) {
+      console.log("Fixing Rose Gold...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Rose Gold");
+      storageData[index].nameEn = "Rose Gold Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData9) {
+      console.log("Fixing Libro...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Libro");
+      storageData[index].nameEn = "Libro Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData10) {
+      console.log("Fixing Manimekalai...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Manimekalai");
+      storageData[index].nameEn = "Manimekhala";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData11) {
+      console.log("Fixing Mushroom Knight...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Mushroom Knight");
+      storageData[index].nameEn = "Mushroom Knight Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData12) {
+      console.log("Fixing Mystictiny...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Mystictiny");
+      storageData[index].nameEn = "Mystic Tiny";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData13) {
+      console.log("Fixing Hedgebat...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Hedgebat");
+      storageData[index].nameEn = "Hedgebat Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData14) {
+      console.log("Fixing Venom...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Venom");
+      storageData[index].nameEn = "Venom Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData15) {
+      console.log("Fixing Vonsharkgon...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Vonsharkgon");
+      storageData[index].nameEn = "Vonshark Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData16) {
+      console.log("Fixing Bonehead...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Bonehead");
+      storageData[index].nameEn = "Bone Head";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData17) {
+      console.log("Fixing Shadow...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Shadow");
+      storageData[index].nameEn = "Shadow Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData18) {
+      console.log("Fixing Serpent Flower...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Serpent Flower");
+      storageData[index].nameEn = "Serpent Flower Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData19) {
+      console.log("Fixing Skelegon...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Skelegon");
+      storageData[index].nameEn = "Skeleton Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData20) {
+      console.log("Fixing Six-leg Horn...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Six-leg Horn");
+      storageData[index].nameEn = "Six-Leg Horn";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData21) {
+      console.log("Fixing Iguagon...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Iguagon");
+      storageData[index].nameEn = "Iguana Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData22) {
+      console.log("Fixing Caracen...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Caracen");
+      storageData[index].nameEn = "Characen";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData23) {
+      console.log("Fixing Catsgon...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Catsgon");
+      storageData[index].nameEn = "Cat Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData24) {
+      console.log("Fixing Qualle...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Qualle");
+      storageData[index].nameEn = "Quale";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData25) {
+      console.log("Fixing Tutankhagon...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Tutankhagon");
+      storageData[index].nameEn = "Tutankha Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData26) {
+      console.log("Fixing Pharaogon...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Pharaogon");
+      storageData[index].nameEn = "Pharaoh Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData27) {
+      console.log("Fixing Enoki Mushroom Dragon...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Enoki Mushroom Dragon");
+      storageData[index].nameEn = "Enoki Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData28) {
+      console.log("Fixing White Gold...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "White Gold");
+      storageData[index].nameEn = "White Gold Dragon";
+      localStorage.setItem("dvcfvs", JSON.stringify(storageData));
+    }
+    if (faultyData29) {
+      console.log("Fixing Hydragon...");
+      const index = storageData.findIndex((obj) => obj.nameEn === "Hydragon");
+      storageData[index].nameEn = "Hydra Dragon";
       localStorage.setItem("dvcfvs", JSON.stringify(storageData));
     }
   }
