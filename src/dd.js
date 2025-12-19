4d181b0bc97a66f48883afd12df529ef3a0ad868
julie <7jujubuilds@gmail.com>
2025-12-19 12:47:15 +0900

    Update database
    - Added partial data for `Rudolph`

--- src/dd.js
+++ src/dd.js
@@ -126,6 +126,7 @@ export const dragonList = [
   // { name: ["Loki", "로키"], traitsEn: ["TraitA", "TraitB"], traitsKo: ["", ""], TraitA: [0, 0, 0, 0], TraitB: [0, 0, 0, 0] },
   { name: ["Rugar", "루가르"], traitsEn: ["Quiet", "Adamant"], traitsKo: ["냉정한", "고집있는"], Quiet: [0, 10, 20, 10], Adamant: [10, 30, 10, 0] },
   { name: ["Lunera", "루네라"], traitsEn: ["Quirky", "Quiet"], traitsKo: ["변덕쟁이", "냉정한"], Quirky: [10, 30, 0, 10], Quiet: [0, 10, 25, 10] },
+  { name: ["Rudolph", "루돌프"], traitsEn: ["Naive", "Unknown"], traitsKo: ["천진난만한", "불명"], Naive: [30, 0, 10, 10], Unknown: [0, 0, 0, 0] },
   { name: ["Rudra", "루드라"], traitsEn: ["Bold", "Hasty"], traitsKo: ["대담한", "성급한"], Bold: [0, 30, 10, 0], Hasty: [30, 10, 10, 0] },
   { name: ["Rude Ore Dragon", "루드오어"], traitsEn: ["Calm", "Quiet"], traitsKo: ["차분한", "냉정한"], Calm: [15, 0, 30, 15], Quiet: [0, 15, 30, 15] },
   { name: ["Lulu", "루루"], traitsEn: ["Naive", "Bold"], traitsKo: ["천진난만한", "대담한"], Naive: [25, 0, 10, 10], Bold: [0, 30, 5, 0] },
